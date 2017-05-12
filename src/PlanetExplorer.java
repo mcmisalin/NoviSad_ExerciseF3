dbec6592d9228ca6dbb00eecfc120d17cfa04ae0
somename <someemail>
2017-05-12 20:14:20 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,12 @@
 // Student ID: it3-2015
 
 public class PlanetExplorer {
+	private int pos_x;
+	private int pos_y;
+	private String obstacles;
+	
+	
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" 
@@ -12,8 +18,12 @@ public class PlanetExplorer {
 		Example use: For a 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 	 */
+		this.gridX=x;
+		this.gridY=y;
+		this.obstacles=obstacles;
 		
-		String [] niz_prepreka = obstacles.split(",");
+		
+		
 		
 		
 		
@@ -34,8 +44,13 @@ public class PlanetExplorer {
 		 *  of the encountered obstacles. No white spaces.
 		 */
 		
-		String [] kretanja = command.toCharArray();		
+		String [] niz_prepreka = obstacles.split(",");
+		for (int i = 0; i < niz_prepreka.length; i++) {
+			System.out.println(niz_prepreka[i]);
+		}
+		char [] kretanja = command.toCharArray();		
 		
+		PlanetExplorer pe = new PlanetExplorer(x, y, command)
 		
 		return null;
 	}
