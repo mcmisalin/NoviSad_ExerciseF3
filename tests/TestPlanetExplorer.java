8327ee46f27d79647f0def771ddb20371fafc407
somename <someemail>
2017-05-12 21:12:18 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void testDaLiRadiOsnovnoNapred() {
+	public void testDaLiRadiOsnovnoNapredN() {
 		PlanetExplorer pe = new PlanetExplorer(10, 10, "",'N',-1,-1);
 		pe.executeCommand("f");
 		assertEquals(" 1 -1",pe.executeCommand("f"));
 	}
 	
 	@Test
-	public void testDaLiRadiOsnovnoNazad() {
+	public void testDaLiRadiOsnovnoNazadN() {
 		PlanetExplorer pe = new PlanetExplorer(10, 10, "",'N',1,1);
 		pe.executeCommand("b");
 		assertEquals(" 9 1", pe.executeCommand("b"));
@@ -47,14 +47,14 @@ public class TestPlanetExplorer {
 	
 	@Test
 	public void testDaLiRadiOsnovnoNazadE() {
-		PlanetExplorer pe = new PlanetExplorer(10, 10, "",'E',2,-1);
+		PlanetExplorer pe = new PlanetExplorer(10, 10, "",'E',0,1);
 		pe.executeCommand("b");
-		assertEquals(" 0 1", pe.executeCommand("b"));
+		assertEquals(" 0 9", pe.executeCommand("b"));
 	}
 	@Test
 	public void testDaLiRadiOsnovnoNapredE() {
-		PlanetExplorer pe = new PlanetExplorer(10, 10, "",'W',0,-1);
+		PlanetExplorer pe = new PlanetExplorer(10, 10, "",'E',0,-1);
 		pe.executeCommand("f");
-		assertEquals(" 0 9", pe.executeCommand("f"));
+		assertEquals(" 0 1", pe.executeCommand("f"));
 	}
 }
