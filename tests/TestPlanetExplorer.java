2e6b53aabef45b239af40cd7068c402d2f918ee7
somename <someemail>
2017-05-12 20:29:23 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -6,7 +6,7 @@ public class TestPlanetExplorer {
 
 	@Test
 	public void testDaLiRadi() {
-		PlanetExplorer pe = new PlanetExplorer(10, 10, "","N");
+		PlanetExplorer pe = new PlanetExplorer(10, 10, "",'N');
 		pe.executeCommand("f");
 		assertEquals("(1,0)",pe.executeCommand("f"));
 	}
